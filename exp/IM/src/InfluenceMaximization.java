58a6e2db9fa9f30b7f8266f8303052678b00a95c
Ningping Wang <iwnking@gmail.com>
2013-12-06 22:28:01 +0800

    load keywords

--- exp/IM/src/InfluenceMaximization.java
+++ exp/IM/src/InfluenceMaximization.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
 import java.util.Set;
 
 /**
@@ -10,8 +13,21 @@ import java.util.Set;
  * To change this template use File | Settings | File Templates.
  */
 public class InfluenceMaximization {
-    public static void readGraph(String filename){
-
+    public static HashMap<Integer, String> loadKeywords(String fname){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            HashMap<Integer, String> map = new HashMap<Integer, String>();
+            String line;
+            while((line = br.readLine())!=null){
+                String[] part = line.split("\t");
+                map.put(Integer.parseInt(part[1]), part[0]);
+            }
+            br.close();
+            return map;
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
     public static void main(String argv[]){
         Graph graph = new Graph();
@@ -23,9 +39,14 @@ public class InfluenceMaximization {
         Set<Integer> seedSet = graph.influenceMaxization(K, theta);
 
         try{
+            HashMap map = loadKeywords(argv[2]);
+            if(map==null){
+                Log.print("Fail to load keywords");
+                return;
+            }
             BufferedWriter bw = new BufferedWriter(new FileWriter("result.txt"));
             for(Integer seed : seedSet){
-                bw.write(seed.toString()+"\n");
+                bw.write(map.get(seed)+"\n");
             }
             bw.close();
         } catch(Exception e){
